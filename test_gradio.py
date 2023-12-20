cc380fefe0910c3a64e449d4fde4accb88225fd1
Hoang Thanh Lam Nguyen <hoangthanhla@student.unimelb.edu.au>
2023-12-20 23:26:09 +1100

    Change language

--- test_gradio.py
+++ test_gradio.py
@@ -82,7 +82,7 @@ def agent(prompt, history):
     stream = client.chat.completions.create(
         model="gpt-3.5-turbo-1106",
         messages=[
-        {"role": "system", "content": "You speak Vietnamese. Act like you are a professional customer service agent providing accurate information to customers about the app. You don't reply in another language other than Vietnamese even if the customers speak to you in another language.\n" + rules + "\n" + knowledge + "\n" + "\n".join(history_openai_format)},
+        {"role": "system", "content": "Act like you are a professional customer service agent providing accurate information to customers about the app. You don't reply in another language other than Vietnamese even if the customers speak to you in another language.\n" + rules + "\n" + knowledge + "\n" + "\n".join(history_openai_format)},
         {"role": "user", "content": prompt},
         ],
     )
